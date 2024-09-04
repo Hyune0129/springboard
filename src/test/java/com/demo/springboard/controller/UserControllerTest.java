ab44860c3d6671119d9701fe2eaa06d8c2e23d4c
Hyune0129 <hyune0129@naver.com>
2024-09-04 16:15:17 +0900

    [Refactor] DTO use builder

--- src/test/java/com/demo/springboard/controller/UserControllerTest.java
+++ src/test/java/com/demo/springboard/controller/UserControllerTest.java
@@ -21,47 +21,48 @@ import com.demo.springboard.service.UserService;
 @WebMvcTest(controllers = UserController.class)
 @DisplayName("Controller Slice test")
 class UserControllerTest {
-    @Autowired
-    MockMvc mockMvc;
+        @Autowired
+        MockMvc mockMvc;
 
-    @MockBean
-    UserService userService;
+        @MockBean
+        UserService userService;
 
-    @Test
-    @DisplayName("get login page")
-    void getLoginPage() throws Exception {
-        mockMvc.perform(get("/user/login"))
-                .andExpect(status().isOk());
-    }
+        @Test
+        @DisplayName("get login page")
+        void getLoginPage() throws Exception {
+                mockMvc.perform(get("/user/login"))
+                                .andExpect(status().isOk());
+        }
 
-    @Test
-    @DisplayName("login process")
-    void login() throws Exception {
-        final String id = "testid";
-        final String password = "testpassword";
-        final int uid = 1;
-        final String name = "test";
-        Map<String, Boolean> errors = new HashMap<>();
+        @Test
+        @DisplayName("login process")
+        void login() throws Exception {
+                final String id = "testid";
+                final String password = "testpassword";
+                final int uid = 1;
+                final String name = "test";
+                Map<String, Boolean> errors = new HashMap<>();
 
-        UserDTO user = new UserDTO();
-        user.setId(id);
-        user.setPassword(password);
-        user.setUid(uid);
-        user.setName(name);
+                UserDTO user = UserDTO.builder()
+                                .id(id)
+                                .password(password)
+                                .uid(uid)
+                                .name(name)
+                                .build();
 
-        when(userService.getUserById(id))
-                .thenReturn(user);
-        when(userService.getBlankError(id, password))
-                .thenReturn(errors);
-        when(userService.getValidateError(password, user))
-                .thenReturn(errors);
+                when(userService.getUserById(id))
+                                .thenReturn(user);
+                when(userService.getBlankError(id, password))
+                                .thenReturn(errors);
+                when(userService.getValidateError(password, user))
+                                .thenReturn(errors);
 
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/user/login")
-                        .param("id", id)
-                        .param("password", password))
-                .andExpect(status().is2xxSuccessful());
+                mockMvc.perform(
+                                MockMvcRequestBuilders.post("/user/login")
+                                                .param("id", id)
+                                                .param("password", password))
+                                .andExpect(status().is2xxSuccessful());
 
-    }
+        }
 
 }
