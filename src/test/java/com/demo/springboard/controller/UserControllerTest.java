517dfde5f5db4a2bf43046acaaf2edf3e8c02316
Hyune0129 <hyune0129@naver.com>
2024-09-10 14:29:20 +0900

    [test] add test services and controller

--- src/test/java/com/demo/springboard/controller/UserControllerTest.java
+++ src/test/java/com/demo/springboard/controller/UserControllerTest.java
@@ -49,8 +49,35 @@ class UserControllerTest {
                 when(userService.getValidateError(password, user)).thenReturn(errors);
 
                 mockMvc.perform(MockMvcRequestBuilders.post("/user/login").param("id", id)
-                                .param("password", password)).andExpect(status().is2xxSuccessful());
+                                .param("password", password))
+                                .andExpect(status().is3xxRedirection());
+                // redirect to login page
 
+        }
+
+        @Test
+        @DisplayName("get register page")
+        void getRegisterPage() throws Exception {
+                mockMvc.perform(get("/user/register")).andExpect(status().isOk());
+        }
+
+        @Test
+        @DisplayName("register process")
+        void register() throws Exception {
+                final String id = "testid1234";
+                final String password = "testpassword11";
+                final String confirm = "testpassword11";
+                final String name = "testname1234";
+
+                UserDTO user = UserDTO.builder().id(id).password(password).name(name).build();
+                Map<String, Boolean> errors = new HashMap<>();
+                when(userService.getBlankError(id, password, name)).thenReturn(errors);
+                when(userService.getUserById(id)).thenReturn(user);
+                when(userService.getValidateError(password, user)).thenReturn(errors);
+
+                mockMvc.perform(MockMvcRequestBuilders.post("/user/register").param("id", id)
+                                .param("password", password).param("confirm", confirm)
+                                .param("name", name)).andExpect(status().is2xxSuccessful());
         }
 
 }
--- src/test/java/com/demo/springboard/service/UserServiceTest.java
+++ src/test/java/com/demo/springboard/service/UserServiceTest.java
@@ -6,14 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import com.demo.springboard.UserDTO;
 import com.demo.springboard.mapper.UserMapper;
 
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
-    @MockBean
+    @Mock
     UserMapper userMapper;
 
     @InjectMocks
@@ -39,10 +42,52 @@ public class UserServiceTest {
         assertTrue(errors.get("isPasswordBlank"));
         assertTrue(errors.get("isNameBlank"));
         errors = userService.getBlankError("testId", "testPassword", "testName");
-        assertFalse(errors.get("isIdBlank"));
-        assertFalse(errors.get("isPasswordBlank"));
-        assertFalse(errors.get("isNameBlank"));
+        assertFalse(errors.containsKey("isIdBlank"));
+        assertFalse(errors.containsKey("isPasswordBlank"));
+        assertFalse(errors.containsKey("isNameBlank"));
     }
 
+    @Test
+    void getValidateErrorTest() {
+        UserDTO user = UserDTO.builder().id("testId1234").password("testPassword1").name("testName")
+                .build();
+        Map<String, Boolean> errors = userService.getValidateError("testPassword1", user);
+        assertFalse(errors.containsKey("isNotMatch"));
+        errors = userService.getValidateError("invalidPassword", user);
+        assertTrue(errors.get("isNotMatch"));
+    }
 
+    @Test
+    void getRegisterValidateErrorTest() {
+        UserDTO user = UserDTO.builder().id("testId1234").password("testPassword1").name("testName")
+                .build();
+        Map<String, Boolean> errors = userService.getRegisterValidateError(user);
+        assertFalse(errors.containsKey("invaildId"));
+        assertFalse(errors.containsKey("invaildPassword"));
+        assertFalse(errors.containsKey("invaildName"));
+        UserDTO invaildUser = UserDTO.builder().id("testId123456789012345678901")
+                .password("invalidPassword1111123132131").name("1111asdf").build();
+        errors = userService.getRegisterValidateError(invaildUser);
+        assertTrue(errors.get("invaildId"));
+        assertTrue(errors.get("invaildPassword"));
+        assertTrue(errors.get("invaildName"));
+    }
+
+    @Test
+    void getDuplicateErrorTest() {
+        UserDTO user = UserDTO.builder().id("testId1234").password("testPassword1").name("testName")
+                .build();
+        UserDTO vaildUser = UserDTO.builder().id("vaildId1234").password("vaildPassword1")
+                .name("vaildName").build();
+        when(userMapper.isExistId("testId1234")).thenReturn(true);
+        when(userMapper.isExistName("testName")).thenReturn(true);
+        when(userMapper.isExistId("vaildId1234")).thenReturn(false);
+        when(userMapper.isExistName("vaildName")).thenReturn(false);
+        Map<String, Boolean> errors = userService.getDuplicateError(user);
+        assertTrue(errors.get("isDuplicatedID"));
+        assertTrue(errors.get("isDuplicatedName"));
+        errors = userService.getDuplicateError(vaildUser);
+        assertFalse(errors.containsKey("isDuplicatedID"));
+        assertFalse(errors.containsKey("isDuplicatedName"));
+    }
 }
